d36f73381a7a134908e64b82050cab39d25b4dc9
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-03-03 11:21:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.camera),
+    ),
+      appBar: AppBar(
+        title: Text("My First Flutter App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Hello World\n\nWelcome to Flutter",
+          style: TextStyle(fontSize: 20.0,
+          fontStyle: FontStyle.italic,
+          color: Colors.red),
+        ),
+      ),
+    );
+  }
+}
+
+
+
